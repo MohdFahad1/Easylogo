54a2e3cfb80b4b7b5c2a503567a0e4310941bdc5
MohdFahad1 <98938137+MohdFahad1@users.noreply.github.com>
2024-08-11 13:01:41 +0530

    feat: Added Hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,10 +1,25 @@
 import React from "react";
 import Logo from "../common/Logo";
+import { Button } from "../ui/button";
 
 const Hero = () => {
   return (
     <main className="p-10 flex flex-col justify-center items-center">
-      <Logo />
+      <div>
+        <Logo />
+      </div>
+      <div className="flex flex-col justify-center items-center text-center gap-8 mt-20">
+        <h1 className="text-7xl font-semibold w-[800px]">
+          Get Your Stunning Logo Instantly, For Free!
+        </h1>
+        <p className="text-xl text-gray-500 w-[500px]">
+          Craft a stunning logo in seconds, no costs, no catch - pure
+          creativity.
+        </p>
+        <Button className="text-xl font-medium p-8">
+          Create your logo - it's free
+        </Button>
+      </div>
     </main>
   );
 };
--- src/components/common/Logo.jsx
+++ src/components/common/Logo.jsx
@@ -4,8 +4,8 @@ import Image from "next/image";
 const Logo = () => {
   return (
     <div className="flex items-center gap-3">
-      <Image src="/logo.png" alt="logo" height={45} width={45} />
-      <h3 className="text-[28px] font-bold">EasyLogo</h3>
+      <Image src="/logo.png" alt="logo" height={43} width={43} />
+      <h3 className="text-[28px] font-bold text-[#111330]">EasyLogo</h3>
     </div>
   );
 };
