e58934c1d010542bc3f1e747c8644e3feac43aba
MohdFahad1 <98938137+MohdFahad1@users.noreply.github.com>
2024-08-15 10:01:52 +0530

    feat: Added headline

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,11 @@ export default function Home() {
     <>
       <Hero />
       <About />
+      <div className="py-10 bg-[#030712] flex justify-center items-center">
+        <h2 className="text-white text-center leading-[60px] text-4xl font-medium w-[650px]">
+          Get a designer-quality logo without the designer price tag
+        </h2>
+      </div>
     </>
   );
 }
