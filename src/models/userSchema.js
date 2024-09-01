addbc0cf4593035ca08483228caf26cf05cf89d6
MohdFahad1 <98938137+MohdFahad1@users.noreply.github.com>
2024-09-01 23:03:57 +0530

    feat: Added user schema

---
+++ src/models/userSchema.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    reuiqred: [true, "Please provide an email"],
+    unique: true,
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  verifyToken: String,
+  verigyTokenExpiry: Date,
+});
+
+const User = mongoose.models.users || mongoose.model("users", userSchema);
+
+export default User;
